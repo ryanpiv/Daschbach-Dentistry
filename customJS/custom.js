d8d0be58062068beda06f76897af025c7317c0b1
Smaktat <ryanpiveteau@gmail.com>
2015-03-15 21:52:36 -0400

    initial commit
    3.15.15, big day of progress.  index pretty much done, might need more
    content

---
+++ customJS/custom.js
@@ -0,0 +1,16 @@
+/*$(function() {
+    $.vegas('slideshow', {
+        delay: 10000,
+        backgrounds: [
+            { src:'images/land7.jpg', fade:1000 },
+            { src:'images/land5.jpg', fade:1000 }
+        ]
+	});
+    $.vegas('overlay', {
+        src:'vegas/overlays/06.png'
+    });
+});*/
+
+$(document).ready(function() {
+	$('.dropdown-toggle').dropdown();
+});
