1c7a86795c7cc60998f135d6c08f21104e3c387e
ryanpiv <ryanpiveteau@gmail.com>
2016-02-06 12:21:45 -0500

    gallery browse stuff
    fixed nav click events for next and prev images
    added resizing to scale to screen ratio
    pretty much made the gallery viewer

--- photo_gallery.php
+++ photo_gallery.php
@@ -14,7 +14,7 @@
       	<button class="filter btn btn-primary" data-filter="all">View All</button>
 
   <?php
-  	ini_set('display_errors', 'On');
+  ini_set('display_errors', 'On');
 
 	$count = 1;
 	$dirArray = array();
@@ -23,14 +23,14 @@
             RecursiveIteratorIterator::SELF_FIRST);
 
 	foreach($iterator as $file) {
-	    if($file->isDir()) {
-	    	if($file->getFilename() != "." && $file->getFilename() != ".."){
-	    		echo "<button class='filter btn btn-primary' data-filter='.category-" . "$count'" . '>' . $file->getFilename(), PHP_EOL . "</button>";
-	    		//array_push($dirArray, $file->getFilename());
-	    		$dirArray[$count] = $file->getFilename();
-	    		$count++;
-	    	}
-	    }
+    if($file->isDir()) {
+    	if($file->getFilename() != "." && $file->getFilename() != ".."){
+    		echo "<button class='filter btn btn-primary' data-filter='.category-" . "$count'" . '>' . $file->getFilename(), PHP_EOL . "</button>";
+    		//array_push($dirArray, $file->getFilename());
+    		$dirArray[$count] = $file->getFilename();
+    		$count++;
+    	}
+    }
 	}
 	echo "<!-- sort controls div end --> </div></div><div class='container container-fill-width-100' id='Container'>";
 	/*
@@ -63,10 +63,10 @@
 
   <div class="modal fade" tabindex="-1" role="dialog"   aria-labelledby="myLargeModalLabel" id="myModal" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div id="displayDiv"></div>
-      </div>
+      <div class="modal-content centered"></div>
     </div>
+    <div onclick="galleryPrevClick()" class="glyphicon glyphicon-chevron-left" aria-hidden="true"></div>
+    <div onclick="galleryNextClick()" class="glyphicon glyphicon-chevron-right" aria-hidden="true"></div>
   </div>
 
   <?php include('footer.php'); ?>
@@ -75,27 +75,88 @@
   <script>
     //debugger;
 
-    $(window).resize(function(){
+    var bg_next;
+    var bg_prev;
+    var bg_current;
+    var bgimage;
+    var image = new Image();
 
+    $(window).resize(function(){
+      resizeImage(image.width, image.height);
     });
 
     $(".mix").click(function(){
       $("#Container").children(".mix").removeClass("clicked");
       $(this).addClass("clicked");
 
-      var bgimage = $(this).children("div").css('background-image');
-      $("#displayDiv").css('background-image', bgimage);
-
-      bgimage = bgimage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-                        .split(',')[0];
-      var image = new Image();
-      image.src = bgimage;
-      var imgWidth = image.width;
-      var imgHeight = image.height;
-      $("#displayDiv").height(imgHeight);
-      $("#displayDiv").width(imgWidth);
-      $("#myModal").modal('show');
+      bgimage = $(this).children("div").css("background-image");
+      bg_current = $(this);
+      bg_next = $(this).next();
+      bg_prev = $(this).prev();
+      
+      setGalleryImage(bgimage);
     });
+
+    $("body").keydown(function(e) {
+      if(e.keyCode == 37) { // left
+        galleryPrevClick();
+      }
+      else if(e.keyCode == 39) { // right
+        galleryNextClick();
+      }
+    });
+
+    function setGalleryImage(bgimage_url){
+      if(bgimage_url == 'undefined' || bgimage_url == '' || typeof(bgimage_url) == 'undefined'){
+        return;
+      }
+      $(".modal-content").css('background-image', bgimage_url);
+
+      bgimage_url = bgimage_url.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+                        .split(',')[0];
+      image.src = bgimage_url;
+
+      if(image.width > window.innerWidth || image.height > window.innerHeight){
+        resizeImage(image.width, image.height);  
+      } else {
+        $(".modal-content").height(image.height);
+        $(".modal-content").width(image.width);
+        $(".modal-dialog").height(image.height);
+        $(".modal-dialog").width(image.width);
+      }
+      
+      $("#myModal").modal('show');
+    }
+
+    function resizeImage(imgWidth, imgHeight){
+      var ratio = Math.min(window.innerWidth / imgWidth, window.innerHeight / imgHeight);
+
+      imgHeight *= ratio;
+      imgWidth *= ratio;
+
+      $(".modal-content").height(imgHeight);
+      $(".modal-content").width(imgWidth);
+      $(".modal-dialog").height(imgHeight);
+      $(".modal-dialog").width(imgWidth);
+    }
+
+    function galleryNextClick(){
+      if(bg_next.length > 0){
+        setGalleryImage($(bg_next).children("div").css("background-image"));
+        bg_current = $(bg_next);
+        bg_next = $(bg_current).next();
+        bg_prev = $(bg_current).prev();  
+      }
+    }
+
+    function galleryPrevClick(){
+      if(bg_prev.length > 0){
+        setGalleryImage($(bg_prev).children("div").css("background-image"));
+        bg_current = $(bg_prev);
+        bg_next = $(bg_current).next();
+        bg_prev = $(bg_current).prev();
+      }
+    }
   </script>
 
   <script>
