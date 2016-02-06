9510ca86be923650c6663294df41dfd92c0c1f04
ryanpiv <ryanpiveteau@gmail.com>
2016-02-06 12:31:29 -0500

    moved files -- reorganized

--- settings.php
+++ settings.php
@@ -3,12 +3,12 @@
 
 <script src="http://code.jquery.com/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
-<script src="mixitup-master/build/jquery.mixitup.min.js" type="text/javascript"></script>
+<script src="js Libraries/mixitup-master/build/jquery.mixitup.min.js" type="text/javascript"></script>
 
 <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
 <link href="http://getbootstrap.com/examples/starter-template/starter-template.css" rel="stylesheet">
 <link href="customCSS/custom.css" rel="stylesheet" type="text/css" />
-<link href="bootstrap-modal-carousel-master/dist/css/bootstrap-modal-carousel.css" rel="stylesheet" />
+<link href="bootstrap/bootstrap-modal-carousel-master/dist/css/bootstrap-modal-carousel.css" rel="stylesheet" />
 <link rel="icon" type="image/png" href="favicon.ico" />
 
-<script src="bootstrap-modal-carousel-master/dist/js/bootstrap-modal-carousel.js"/></script/>
+<script src="bootstrap/bootstrap-modal-carousel-master/dist/js/bootstrap-modal-carousel.js"/></script/>
